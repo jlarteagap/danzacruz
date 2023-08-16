f60b55a2f71399901220772ee8f3fb7144bb2a6b
Jorge <jorgelarteagap@gmail.com>
2023-08-16 11:55:48 -0400

    [FEAT] Center form

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,14 @@ import { AddParticipant } from '../src/components/addParticipan/addParticipant'
 export default function Home() {
   return (
     <main className="container">
-      <AddParticipant />
+      <div className="is-flex is-justify-content-center has-text-weight-bold">
+        <h2 className="is-size-3 my-4">Formulario de registro</h2>
+      </div>
+      <div className="card m-auto" style={{ width: '50%' }}>
+        <div className="card-content">
+          <AddParticipant />
+        </div>
+      </div>
     </main>
   )
 }
