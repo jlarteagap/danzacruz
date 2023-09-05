82072357e555cf1269ad41d81e73cc4c9f50ba0d
Jorge <jorgelarteagap@gmail.com>
2023-09-04 20:18:22 -0400

    [FIX] Data

--- src/components/email/sendEmail.js
+++ src/components/email/sendEmail.js
@@ -1,11 +1,18 @@
 import emailjs from '@emailjs/browser'
+import { Certificade } from '../pdf/certificade'
+import { pdf } from '@react-pdf/renderer'
 
 export const SendEmail = values => {
+  const certificade = <Certificade name={values.name} />
+  const attachCertificade = pdf(certificade).toBlob()
+  const transformedAttach = attachCertificade.toDataURL()
+
   const dataEmail = {
     to_name: values.name,
     reply_to: values.email,
-    message: 'lorem ipsun Festival danzacruz'
+    content: transformedAttach
   }
+
   emailjs
     .send('service_ce9y3ij', 'template_ne3495f', dataEmail, '93BAteTRApolzmkNo')
     .then(
