4c1a47fb468c717d0f3d8f149b6624b2a2645667
Jorge <jorgelarteagap@gmail.com>
2023-09-07 16:17:29 -0400

    [FIX] Change a value of categories

--- src/components/addParticipan/utils.js
+++ src/components/addParticipan/utils.js
@@ -10,43 +10,43 @@ export const modalidad = [
   { value: 'Musical', name: 'Musical' },
   { value: 'Jazz', name: 'Jazz' },
   { value: 'Street Dance', name: 'Street Dance' },
-  { value: 'Folklore Departamental', name: 'Folklore Departamental' },
-  { value: 'Folklore Nacional', name: 'Folklore Nacional' },
-  { value: 'Folklore Internacional', name: 'Folklore Internacional' },
-  { value: 'Folklore Etnico y de Raíz', name: 'Folklore Etnico y de Raíz' },
+  { value: 'Folk Departamental', name: 'Folklore Departamental' },
+  { value: 'Folk Nacional', name: 'Folklore Nacional' },
+  { value: 'Folk Internacional', name: 'Folklore Internacional' },
+  { value: 'Folk Etnico y de Raíz', name: 'Folklore Etnico y de Raíz' },
   { value: 'K Pop', name: 'K Pop' }
 ]
 
 export const colegios = [
   {
-    value: 'Pre-infantil: 3 a 5 años y 11 meses',
-    name: 'Pre-infantil: 3 a 5 años y 11 meses'
+    value: 'Pre-infantil',
+    name: 'Pre-infantil'
   },
   {
-    value: 'Infantil: De 6 a 11 años y 11 meses',
-    name: 'Infantil: De 6 a 11 años y 11 meses'
+    value: 'Infantil',
+    name: 'Infantil'
   },
   {
-    value: 'Juvenil: De 12 a 18 años',
-    name: 'Juvenil: De 12 a 18 años'
+    value: 'Juvenil',
+    name: 'Juvenil'
   }
 ]
 export const general = [
   {
-    value: 'Pre-infantil: 3 a 5 años y 11 meses',
-    name: 'Pre-infantil: 3 a 5 años y 11 meses'
+    value: 'Pre-infantil',
+    name: 'Pre-infantil'
   },
   {
-    value: 'Infantil: De 6 a 11 años y 11 meses',
-    name: 'Infantil: De 6 a 11 años y 11 meses'
+    value: 'Infantil',
+    name: 'Infantil'
   },
   {
-    value: 'Juvenil: De 12 a 17 años y 11 meses',
-    name: 'Juvenil: De 12 a 17 años y 11 meses'
+    value: 'Juvenil',
+    name: 'Juvenil'
   },
   {
-    value: 'Adulto: De 18 años en adelante',
-    name: 'Adulto: De 18 años en adelante'
+    value: 'Adulto',
+    name: 'Adulto'
   }
 ]
 export const category = ['General', 'Colegios']
