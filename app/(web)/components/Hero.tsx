f8747476c40e7979b5727a7ee9b60c6134c14fb9
Jorge <jorgelarteagap@gmail.com>
2025-06-17 23:46:31 -0400

    [FEAT] change for new year of dance

--- app/(web)/components/Hero.tsx
+++ app/(web)/components/Hero.tsx
@@ -1,55 +1,54 @@
-import { Button } from '@/components/ui/button'
-import { MessageSquareShare } from 'lucide-react'
-import Image from 'next/image'
-import Link from 'next/link'
+import { Button } from "@/components/ui/button";
+import { MessageSquareShare } from "lucide-react";
+import Link from "next/link";
 
 export default function Hero() {
   return (
-    <section className="h-svh flex justify-center items-center flex-col relative bg-gradient-to-r from-fuchsia-300 bg-blend-multiply">
-      <div className="flex justify-center items-center w-full flex-col">
-        <h2 className="text-4xl font-bold uppercase relative md:text-[72px] text-fuchsia-900 filter drop-shadow-2xl">
+    <section className='h-svh flex justify-center items-center flex-col relative bg-gradient-to-r from-slate-300 bg-blend-multiply'>
+      <div className='flex justify-center items-center w-full flex-col'>
+        <h2 className='text-4xl font-bold uppercase relative md:text-[72px] text-slate-700 filter drop-shadow-2xl'>
           Danzacruz
-          <span className="font-normal rotate-90 absolute text-xs top-0 bottom-0 md:text-2xl">
-            2024
+          <span className='font-normal rotate-90 absolute text-xs top-0 bottom-0 md:text-2xl'>
+            2025
           </span>
         </h2>
-        <p className="mt-3 md:text-2xl text-white">
-          24 años Proyectando Bolivia al mundo
+        <p className='mt-3 md:text-2xl text-slate-400'>
+          25 años Proyectando Bolivia al mundo
         </p>
       </div>
-      <div className="absolute w-full h-screen -z-10">
-        <Image
-          src="/img/dc-2024.jpg"
-          className="object-cover"
+      <div className='absolute w-full h-screen -z-10'>
+        {/* <Image
+          src='/img/dc-2024.jpg'
+          className='object-cover'
           fill={true}
-          alt="Danzacruz 2024"
-        />
+          alt='Danzacruz 2024'
+        /> */}
       </div>
-      <div className="uppercase md:text-xl mt-10 text-fuchsia-900">
-        Del 8 al 10 de noviembre
+      <div className='uppercase md:text-xl mt-10 text-slate-600'>
+        9 de 12 de Octubre del 2025
       </div>
-      <div className="mt-5 gap-3 flex">
-        <Button className="">
+      <div className='mt-5 gap-3 flex'>
+        <Button className='bg-slate-600 hover:bg-slate-300 transition-all hover:text-slate-700'>
           <Link
-            href="http://wa.me/59175020012"
-            target="_blank"
-            className="flex"
+            href='http://wa.me/59175020012'
+            target='_blank'
+            className='flex'
           >
-            <MessageSquareShare className="mr-2" />
+            <MessageSquareShare className='mr-2' />
             75020012
           </Link>
         </Button>
-        <Button>
+        <Button className='bg-slate-600 hover:bg-slate-300 transition-all hover:text-slate-700'>
           <Link
-            href="http://wa.me/59175553576"
-            target="_blank"
-            className="flex"
+            href='http://wa.me/59175553576'
+            target='_blank'
+            className='flex'
           >
-            <MessageSquareShare className="mr-2" />
+            <MessageSquareShare className='mr-2' />
             75553576
           </Link>
         </Button>
       </div>
     </section>
-  )
+  );
 }
--- app/(web)/components/Layout/Footer.tsx
+++ app/(web)/components/Layout/Footer.tsx
@@ -1,38 +1,42 @@
 export function Footer() {
   return (
-    <footer className="bg-slate-800 text-white py-7">
-      <div className="container mx-auto flex flex-col md:flex-row md:gap-5 px-4">
-        <div className="w-full md:w-1/3">
-          <h2 className="text-2xl text-slate-50 font-bold">
+    <footer className='bg-slate-900 text-slate-500 py-7'>
+      <div className='container mx-auto flex flex-col md:flex-row md:gap-5 px-4'>
+        <div className='w-full md:w-1/3'>
+          <h2 className='text-xl text-slate-300 font-bold'>
             Acerca del Festival Danzacruz
           </h2>
-          <p className="pt-4">24 años proyectando Bolivia al mundo</p>
+          <p className='pt-4'>25 años proyectando Bolivia al mundo</p>
         </div>
-        <div className="w-full ">
+        <div className='w-full '>
           <div>
-            <h4 className="text-2xl font-semibold">Redes Sociales</h4>
+            <h4 className='text-xl font-semibold text-slate-300'>
+              Redes Sociales
+            </h4>
             <div>
               <a
-                href="https://www.facebook.com/festivaldanzacruz"
-                target="_blank"
-                className="hover:text-fuchsia-600 transition-all"
+                href='https://www.facebook.com/festivaldanzacruz'
+                target='_blank'
+                className='hover:text-slate-400 transition-all'
               >
                 Facebook
               </a>
             </div>
           </div>
           <div>
-            <h4 className="text-2xl font-semibold">Contacto</h4>
-            <div className="flex flex-col">
+            <h4 className='text-xl font-semibold mt-5 text-slate-300'>
+              Contacto
+            </h4>
+            <div className='flex flex-col'>
               <a
-                href="http://wa.me/59175020012"
-                className="hover:text-fuchsia-600 transition-all"
+                href='http://wa.me/59175020012'
+                className='hover:text-slate-400 transition-all'
               >
                 75020012
               </a>
               <a
-                href="http://wa.me/59175553576"
-                className="hover:text-fuchsia-600 transition-all"
+                href='http://wa.me/59175553576'
+                className='hover:text-slate-400 transition-all'
               >
                 75553576
               </a>
@@ -41,5 +45,5 @@ export function Footer() {
         </div>
       </div>
     </footer>
-  )
+  );
 }
