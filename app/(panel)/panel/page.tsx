dec65d0b1fdc9a510e858946acf5335cbebbba71
Jorge <jorgelarteagap@gmail.com>
2025-10-06 01:43:36 -0400

    [FIX] firebase delete panel old version

--- app/(panel)/panel/page.tsx
+++ app/(panel)/panel/page.tsx
@@ -4,8 +4,8 @@
 export default function Dashboard() {
   return (
     <div>
-      <div className="text-2xl mb-4 uppercase font-semibold">DanzaCruz</div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-x-20">
+      <div className='text-2xl mb-4 uppercase font-semibold'>DanzaCruz</div>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-x-20'>
         {/* <CardSumary icon={UserRound} title="Companies" />
         <CardSumary icon={UserRound} title="Companies" />
         <CardSumary icon={UserRound} title="Companies" /> */}
@@ -15,5 +15,5 @@ export default function Dashboard() {
         {/* <CardSumary /> */}
       </div>
     </div>
-  )
+  );
 }
