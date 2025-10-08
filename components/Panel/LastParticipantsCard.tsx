b568cfb7e3ffb129f88a2610e7ed6f6a6ad5775d
Jorge <jorgelarteagap@gmail.com>
2025-10-08 15:21:48 -0400

    [FEAT] refactorice and menu resize

--- components/Panel/LastParticipantsCard.tsx
+++ components/Panel/LastParticipantsCard.tsx
@@ -10,7 +10,7 @@ import {
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Users } from "lucide-react";
-import { Participant } from "@/types/userPanel.types"; // ⚡ Ajusta la ruta de tus tipos
+import { Participant } from "@/types/userPanel.types";
 
 interface Props {
   participants: Participant[];
@@ -18,7 +18,10 @@ interface Props {
 
 export default function LastParticipantsCard({ participants }: Props) {
   const lastFive = [...participants]
-    .sort((a, b) => b.createdAt - a.createdAt) // ⚡ Cambia por createdAt si lo agregas
+    .sort(
+      (a, b) =>
+        new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+    )
     .slice(0, 5);
 
   return (
